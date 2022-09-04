b40803316c1bb2e3f8dc87c51df2ca3e980a0c66
MaLin <ml2907957301@163.com>
2022-09-04 21:52:03 +0800

    Initial commit

---
+++ spring_demo/src/main/java/com/atmalin/service/UserService.java
@@ -0,0 +1,12 @@
+package com.atmalin.service;
+
+/**
+ * @version v1.0
+ * @ClassName: UserService
+ * @Description: 业务逻辑层接口
+ * @Author: 黑马程序员
+ */
+public interface UserService {
+
+    public void add();
+}
